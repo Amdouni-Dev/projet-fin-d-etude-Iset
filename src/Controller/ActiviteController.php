799e098a1bf42deb0dabc7f5b85b58d5bb1fa11b
Mouna-Amdouni <mounaamdouni213@gmail.com>
2021-06-21 19:28:10 +0100

    exception activites

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -29,6 +29,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Image;
 
 
 /**
@@ -188,7 +189,12 @@ try{
             ])
 
 
-            ->add('video', FileType::class, ['label' => 'Chargez votre video'])
+            ->add('video', FileType::class, ['mapped'=>false,
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '5M',
+                    ])
+              ],'label' => 'Chargez votre video'])
             ->getForm();
         $form->handleRequest($request);
 
